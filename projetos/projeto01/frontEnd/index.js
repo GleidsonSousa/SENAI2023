68499ff890ebc9be9c9dc2a8f2ea40c59f1ad97a
Gleisin <gleidson2090@gmail.com>
2023-01-29 15:56:22 -0300

    new att

--- projetos/projeto01/frontEnd/index.js
+++ projetos/projeto01/frontEnd/index.js
@@ -3,6 +3,8 @@ const url = 'http://localhost:4500/pedidos/read';
 let ped = [];
 
 const divPedidos = document.querySelector('#eaC');
+const divPedidos2 = document.querySelector('#eaB');
+
 
 
 function onLoad() {
@@ -25,13 +27,14 @@ function preencher() {
         var novoCardPedido = divPedidos.cloneNode(true)
         novoCardPedido.classList.remove('model')
 
+        var novoCardPedido2 = divPedidos2.cloneNode(true)
+        novoCardPedido2.classList.remove('model')
+
         
 
         // ESTA A CAMINHO
-        console.log(p.hora_entrega)
 
-        if (p.hora_entrega == "00:00:00") {
-            console.log(p.hora_entrega)
+        if (p.hora_entrega == "00:00:00" ) {
 
 
             novoCardPedido.querySelector('#idPed').innerHTML = p.id_pedido
@@ -40,27 +43,104 @@ function preencher() {
             novoCardPedido.querySelector('#end').innerHTML = p.endereco
             novoCardPedido.querySelector('#date').innerHTML = p.data
             novoCardPedido.querySelector('#hip').innerHTML = p.hora_pedido
+            novoCardPedido.querySelector("#btnCancelar").addEventListener("click", () => { remover(p.id_pedido, novoCardPedido); })
+            novoCardPedido.querySelector("#btnEnvia").addEventListener("click", () => { updateEntrega(p.id_pedido); })
             document.querySelector('.pedidosem').appendChild(novoCardPedido)
         }
 
-        // if (p.hora_entrega == "") {
+        if (p.hora_fim == "00:00:00" && p.hora_entrega != "00:00:00") {
 
-        //     novoCardPedido.querySelector('.idPedido').innerHTML = p.id_pedido
-        //     novoCardPedido.querySelector('.nome_cliente').innerHTML = p.cliente
-        //     novoCardPedido.querySelector('.hora_pedido').innerHTML = p.hora_pedido
-        //     novoCardPedido.querySelector('.endereco').innerHTML = p.endereco
-        //     novoCardPedido.querySelector('.cidade').innerHTML += p.cidade
-        //     novoCardPedido.querySelector('.bairro').innerHTML = p.bairro
-        //     novoCardPedido.querySelector('.produto').innerHTML = p.produto
-        //     novoCardPedido.querySelector('.preco_total').innerHTML = "R$ " + p.preco
-        //     novoCardPedido.querySelector('.qtd').innerHTML = p.quantidade + " u - "
-        //     document.querySelector('.andamento').appendChild(novoCardPedido)
-        // }
+            console.log(p.hora_entrega)
+
+
+            novoCardPedido2.querySelector('#idPed').innerHTML = p.id_pedido
+            novoCardPedido2.querySelector('#nCli').innerHTML = p.cliente
+            novoCardPedido2.querySelector('#prod').innerHTML = p.produto
+            novoCardPedido2.querySelector('#end').innerHTML = p.endereco
+            novoCardPedido2.querySelector('#date').innerHTML = p.data
+            novoCardPedido2.querySelector('#hip').innerHTML = p.hora_pedido
+            novoCardPedido2.querySelector('#h').innerHTML = p.hora_entrega
+            novoCardPedido2.querySelector("#btnEnvia").addEventListener("click", () => { updateEntregue(p.id_pedido); })
+
+            document.querySelector('.pedidosca').appendChild(novoCardPedido2)
+        }
 
 
     })
 }
+ function updateEntregue(id){
+    const body = {
+        "id_pedido":id
+    }
+    const options = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' }
+    };
+    options.body = JSON.stringify(body);
+    fetch("http://localhost:4500/pedidos/update-finalizado", options)
+        .then(resp => resp.status)
+        .then(resp => {
+            if (resp == 200) {
 
+                alert(" Entregue com Sucesso 😀 ✔")
+                window.location.reload()
+
+
+            } else {
+                alert("Erro ao Entregar pedido 🙁❌: " + resp)
+            }
+        })
+        .catch(err => {
+            alert("❌ Erro ao enviar. Erro:" + err)
+
+
+        });
+}
+
+
+
+function updateEntrega(id){
+    const body = {
+        "id_pedido":id,
+        "entregador":1
+    }
+    const options = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' }
+    };
+    options.body = JSON.stringify(body);
+    fetch("http://localhost:4500/pedidos/update-entregador", options)
+        .then(resp => resp.status)
+        .then(resp => {
+            if (resp == 200) {
+
+                alert(" Enviado com Sucesso 😀 ✔")
+                window.location.reload()
+
+
+            } else {
+                alert("Erro ao enviar dados 🙁❌: " + resp)
+            }
+        })
+        .catch(err => {
+            alert("❌ Erro ao enviar dados. Erro:" + err)
+
+
+        });
+}
+       
+
+function remover(id, novoCardPedido) {
+    fetch("http://localhost:4500/pedidos/delete/" + id, {
+        "method":"DELETE"
+    })
+    .then(resp => {})
+    .then(p => {
+        novoCardPedido.remove();
+        window.location.reload()
+
+    });
+}
 
 function GerarPedido(){
     let data = {}
